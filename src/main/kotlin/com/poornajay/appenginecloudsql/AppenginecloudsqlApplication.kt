5e532440065a961e7748a1185cc1b97560df42df
Poorna Jayasinghe <poornasjayasinghe@gmail.com>
2020-11-08 17:16:46 +1100

    Initial

---
+++ src/main/kotlin/com/poornajay/appenginecloudsql/AppenginecloudsqlApplication.kt
@@ -0,0 +1,11 @@
+package com.poornajay.appenginecloudsql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppenginecloudsqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppenginecloudsqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/poornajay/appenginecloudsql/AppenginecloudsqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.poornajay.appenginecloudsql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppenginecloudsqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
